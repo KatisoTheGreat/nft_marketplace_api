7268b5287eaff1e484d7801ba5874b1fde6ee060
KatisoTheGreat <katisoledwaba@gmail.com>
2023-08-25 15:35:54 +0200

    added get single nft api

--- app.js
+++ app.js
@@ -22,18 +22,42 @@ app.get("/api/v1/nfts", (req, res) => {
 //POST METHOD
 
 app.post("/api/v1/nfts", (req, res) => {
+    // console.log(req.body);
+    // res.send("Done posting");
 
-        const newId = nfts[nfts.length - 1] +1;
-        const newNFTs = Object.assign({id: newId}, req.body);
+    const newId = nfts[nfts.length - 1].id + 1;
+    const newNFTs = Object.assign({id: newId}, req.body);
 
-        nfts.push(newNFTs);
+    nfts.push(newNFTs);
 
-        fs.writeFileSync(`${__dirname}/nft-data/data/nft-sample.json`, JSON.stringify(nfts), (err) => {
+    fs.writeFileSync(`${__dirname}/nft-data/data/nft-sample.json`, JSON.stringify(nfts), (err) => {
             res.status(201).json({
                 status: "success",
                 nft: newNFTs
-            });
         });
+    });
+});
+
+// GET SINGLE NFT
+
+app.get("/api/v1/nfts/:id", (req, res) =>{
+
+    const id = req.params.id * 1;
+    const nft = nfts.find(element => element.id === id);
+
+    if (!nft) {
+        res.status(404).json({
+            status: "failed",
+            message: "Invalied ID"
+        });
+    }
+    
+    res.status(200).json({
+        status: "success",
+        data: {
+            nft: nft
+        }
+    });
 });
 
 const port = 3000;
